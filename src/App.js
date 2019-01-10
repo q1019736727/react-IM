2a40a7439456ad6e3d99aa8e445f3ea1e295b7ff
Chiu Yung <1837060380@qq.com>
2019-01-10 13:48:25 +0800

    babel-plugin-transform-decorators-legacy 修饰器配置

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function log() {
+
+}
+
+@log
 class App extends Component {
   render() {
     return (
